0cd17d09ae78c2a14e76b42ac66d05d2917df874
Constantine Khroulev <ckhroulev@alaska.edu>
2012-01-30 10:07:49 -0900

    Made some progress. (It does something; not sure what, though.)

--- drainagebasin.cc
+++ drainagebasin.cc
@@ -1,4 +1,9 @@
 #include <cstdio>
+#include "drainagebasin.hh"
+#include <vector>
+#include <cmath>
+
+using namespace std;
 
 // MPI stuff, NetCDF I/O
 // #include <mpi.h>
@@ -6,31 +11,62 @@
 
 
 int main(int argc, char **argv) {
-  gsl_odeiv2_system system = {function, jacobian, 2, NULL};
 
-  gsl_odeiv2_driver * d =
-    gsl_odeiv2_driver_alloc_y_new (&system,
-                                   gsl_odeiv2_step_bsimp,
-                                   1e-6, 1e-6, 0.0);
-  int i;
-  double t = 0.0, t1 = 100.0;
-  double y[2] = { 0.0, 0.0 };
+  vector<double> X(2), Y(2), Z(4);
 
-  printf("set grid; plot \"-\" with points title \"ODE system solution\"\n");
-  for (i = 1; i <= 100; i++)
-    {
-      double ti = i * t1 / 100.0;
-      int status = gsl_odeiv2_driver_apply (d, &t, ti, y);
+  X[0] = -1;
+  X[1] = 1;
+  Y[0] = -1;
+  Y[1] = 1;
 
-      if (status != GSL_SUCCESS)
-	{
-	  printf ("error, return value=%d\n", status);
-	  break;
-	}
+  gsl_matrix_view z_view = gsl_matrix_view_array(&Z[0], 2, 2);
+  gsl_matrix * m = &z_view.matrix;
 
-      printf ("%.5e %.5e\n", y[0], y[1]);
+  gsl_matrix_set(m, 0, 0,  1);
+  gsl_matrix_set(m, 0, 1, -1);
+  gsl_matrix_set(m, 1, 1,  1);
+  gsl_matrix_set(m, 1, 0, -1);
+
+  DEM_Bilinear dem(&X[0], 2, &Y[0], 2, &Z[0]);
+
+  gsl_odeiv2_system system = {function, jacobian, 2, &dem};
+
+  gsl_odeiv2_step *step = gsl_odeiv2_step_alloc(gsl_odeiv2_step_bsimp, 2);
+
+  vector<double> y(2), y_old(2), yerr(2);
+  double tol = 1e-3;
+
+  y[0] = -0.5;
+  y[1] = -0.5;
+  y_old = y;
+
+  printf("set xrange [%f:%f];\n"
+         "set yrange [%f:%f]\n"
+         "set grid;\n"
+         "plot \"-\" with linespoints title \"ODE system solution\"\n",
+         X[0], X[1],
+         Y[0], Y[1]);
+
+  for (;;) {
+    int status = gsl_odeiv2_step_apply(step, 0, tol, &y[0], &yerr[0], NULL, NULL, &system);
+
+    if (status != GSL_SUCCESS)
+      {
+        printf ("error, return value=%d\n", status);
+        break;
+      }
+
+    printf ("%.5e %.5e\n", y[0], y[1]);
+
+    if (sqrt((y[0] - y_old[0])*(y[0] - y_old[0]) +
+             (y[1] - y_old[1])*(y[1] - y_old[1])) < tol) {
+      break;
     }
 
-  gsl_odeiv2_driver_free (d);
+    y_old = y;
+
+  }
+
+  gsl_odeiv2_step_free (step);
   return 0;
 }
--- system.cc
+++ system.cc
@@ -1,42 +1,85 @@
-// GSL stuff
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_matrix.h>
-#include <gsl/gsl_odeiv2.h>
+#include "drainagebasin.hh"
 
-class Context {
-  Context(double *x, int Mx, double *y, int My);
-  ~Context();
-public:
-  double one_over_dx, one_over_dy;
-  double *x, double *y;
-  int Mx, My;
-  gsl_interp_accel *x_accel, *y_accel;
-};
-
-Context::Context(double *my_x, int my_Mx, double *my_y, int my_My) {
+DEM_Bilinear::DEM_Bilinear(double *my_x, int my_Mx, double *my_y, int my_My, double *my_z) {
   x_accel = gsl_interp_accel_alloc();
   y_accel = gsl_interp_accel_alloc();
 
   x = my_x;
   Mx = my_Mx;
-  one_over_dx = 1.0 / (x[1] - x[0]);
 
   y = my_y;
   My = my_My;
+
+  z = my_z;
+
+  // We assume that the grid is equally-spaced.
+  one_over_dx = 1.0 / (x[1] - x[0]);
   one_over_dy = 1.0 / (y[1] - y[0]);
 }
 
-Context::~Context() {
+DEM_Bilinear::~DEM_Bilinear() {
   gsl_interp_accel_free(x_accel);
   gsl_interp_accel_free(y_accel);
+}
+
+void DEM_Bilinear::eval(double x0, double y0,
+                        double f[], double jac[]) {
+  int i, j;
+
+  if (x[0] > x0 || x0 >= x[Mx-1] ||
+      y[0] > y0 || y0 >= y[My-1]) {
+
+    // printf("x = %f, y = %f\n", x0, y0);
+
+    if (f != NULL) {
+      f[0] = f[1] = 0;
+    }
+
+    if (jac != NULL) {
+      jac[0] = jac[1] = jac[2] = jac[3] = 0;
+    }
+
+    return;
+  }
+
+  i = gsl_interp_accel_find(x_accel, x, Mx, x0);
+  j = gsl_interp_accel_find(y_accel, y, My, y0);
+
+  gsl_matrix_view z_view = gsl_matrix_view_array(z, My, Mx);
+  gsl_matrix * m = &z_view.matrix;
+  double
+    A = gsl_matrix_get(m, i,     j),
+    B = gsl_matrix_get(m, i,     j + 1),
+    C = gsl_matrix_get(m, i + 1, j + 1),
+    D = gsl_matrix_get(m, i + 1, j);
+
+  if (f != NULL) {
+    double
+      alpha = one_over_dx * (x[i] - x0),
+      beta  = one_over_dy * (y[j] - y0);
+
+    f[0] = - one_over_dx * ((1 - beta)  * (D - A) + beta  * (C - B));
+    f[1] = - one_over_dy * ((1 - alpha) * (B - A) + alpha * (C - D));
+  }
+
+  if (jac != NULL) {
+    // J(i,j) = dfdy[i * dimension + j]
+    // J(i,j) = \frac{\partial f_i}{\partial y_j}
+
+    double jac_factor = one_over_dx * one_over_dy * (A + C - B - D);
+    jac[0 * 2 + 0] = 0;
+    jac[0 * 2 + 1] = jac_factor;
+    jac[1 * 2 + 0] = jac_factor;
+    jac[1 * 2 + 1] = 0;
+  }
 }
 
 int function(double t, const double y[], // inputs
              double f[],                 // output
              void* params) {             // optional input/output
 
-  f[0] = 1.0;
-  f[1] = 1.0;
+  DEM_Bilinear *dem = (DEM_Bilinear*)params;
+  dem->eval(y[0], y[1], f, NULL);
 
   return GSL_SUCCESS;
 }
@@ -44,13 +87,9 @@ int function(double t, const double y[], // inputs
 int jacobian(double t, const double y[],  // inputs
              double *dfdy, double dfdt[], // outputs
              void *params) {              // optional input/output
-  // J(i,j) = dfdy[i * dimension + j]
-  // J(i,j) = \frac{\partial f_i}{\partial y_j}
 
-  dfdy[0 * 2 + 0] = 0;
-  dfdy[0 * 2 + 1] = 0;
-  dfdy[1 * 2 + 0] = 0;
-  dfdy[1 * 2 + 1] = 0;
+  DEM_Bilinear *dem = (DEM_Bilinear*)params;
+  dem->eval(y[0], y[1], NULL, dfdy);
 
   dfdt[0] = 0;
   dfdt[1] = 0;

b49f75635313e01d6b6f3f8716ef8e12bf5ed66e
Constantine Khroulev <ckhroulev@alaska.edu>
2012-02-16 18:10:30 -0900

    Fixed a typo/updated basins.pyx.

--- python/basins.pyx
+++ python/basins.pyx
@@ -2,7 +2,7 @@
 import numpy as np
 cimport numpy as np
 
-cimport basins
+cimport basins_c
 
 ctypedef np.double_t double_t
 
@@ -19,6 +19,8 @@ def basins(np.ndarray[dtype=double_t, ndim=1] x,
     """
     cdef np.ndarray[dtype=double_t, ndim=2, mode="c"] output
 
+    # z and mask are typed, so z.shape is not a Python object.
+    # This means that we have to compare z.shape[0,1] to mask.shape[0,1] 'by hand'.
     if not (z.shape[0] == mask.shape[0] and z.shape[1] == mask.shape[1]):
         raise ValueError("arguments z and mask have to have the same shape: got (%d,%d) and (%d,%d)" %
                          (z.shape[0], z.shape[1], mask.shape[0], mask.shape[1]))
@@ -34,9 +36,9 @@ def basins(np.ndarray[dtype=double_t, ndim=1] x,
     else:
         output = mask.copy()
 
-    basins.basins(<double*>x.data, x.size,
-                  <double*>y.data, y.size,
-                  <double*>z.data,
-                  <double*>output.data)
+    basins_c.basins(<double*>x.data, x.size,
+                    <double*>y.data, y.size,
+                    <double*>z.data,
+                    <double*>output.data)
 
     return output

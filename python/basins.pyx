bdfe024b79d32b2275860127285b5d5b38be2eee
Constantine Khroulev <ckhroulev@alaska.edu>
2012-02-21 14:06:24 -0900

    Specified type sizes. Seems to help.

--- python/basins.pyx
+++ python/basins.pyx
@@ -4,8 +4,8 @@ import numpy as np
 
 cimport basins_c
 
-ctypedef np.double_t double_t
-ctypedef np.int_t int_t
+ctypedef np.float64_t double_t
+ctypedef np.int32_t int_t
 
 def init_mask(np.ndarray[dtype=double_t, ndim=2, mode="c"] thk):
     """
@@ -17,7 +17,7 @@ def init_mask(np.ndarray[dtype=double_t, ndim=2, mode="c"] thk):
     Mx = thk.shape[1]
     My = thk.shape[0]
 
-    mask = np.zeros((thk.shape[0], thk.shape[1]), dtype=np.int)
+    mask = np.zeros((thk.shape[0], thk.shape[1]), dtype=np.int32)
 
     basins_c.init_mask(Mx, My, <double*>thk.data, <int*> mask.data)
 
